44ae57c1090943af6d9be214b9478cb0c862822b
semihalev <semihalev@gmail.com>
2025-05-31 19:07:25 +0300

    Update version information

--- internal/common/version.go
+++ internal/common/version.go
@@ -23,7 +23,7 @@ const (
 	// VersionPatch is the patch version of the driver
 	VersionPatch = "8"
 	// VersionSuffix is the suffix of the driver version
-	VersionSuffix = "58d0c016" // git commit hash
+	VersionSuffix = "3c1d1803" // git commit hash
 
 	// VersionString is the version string of the driver
 	VersionString = "Stoolap v" + VersionMajor + "." + VersionMinor + "." + VersionPatch + "-" + VersionSuffix
