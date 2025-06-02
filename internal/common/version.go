2ed5df7f9af722d233cfed12afb71ccc3cb3c28e
semihalev <semihalev@gmail.com>
2025-06-02 09:20:23 +0300

    Added bug scenario and verify fixes

--- internal/common/version.go
+++ internal/common/version.go
@@ -23,7 +23,7 @@ const (
 	// VersionPatch is the patch version of the driver
 	VersionPatch = "0"
 	// VersionSuffix is the suffix of the driver version
-	VersionSuffix = "f15f68b6" // git commit hash
+	VersionSuffix = "6494f99c" // git commit hash
 
 	// VersionString is the version string of the driver
 	VersionString = "Stoolap v" + VersionMajor + "." + VersionMinor + "." + VersionPatch + "-" + VersionSuffix
